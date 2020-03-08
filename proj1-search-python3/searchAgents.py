95dc02abebbd904bae2dbe63c41f2c7a82db39c3
Julian <juliancastrence@gmail.com>
2020-03-07 20:03:42 -0500

    8

--- proj1-search-python3/searchAgents.py
+++ proj1-search-python3/searchAgents.py
@@ -516,7 +516,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.bfs(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -552,7 +552,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
